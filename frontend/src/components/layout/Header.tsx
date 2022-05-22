80848f28e560e14c45ef9933ab9915e05d03182b
akariyan <fntlsltm@gmail.com>
2022-05-22 22:10:26 +0900

    feat: add default route and layout

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="fixed w-full h-24 flex flex-row items-center border-b-2 border-gray-200">
+      <img src="logo_long_transparents.png" className="h-16 ml-5" />
+    </header>
+  );
+};
+
+export default Header;
