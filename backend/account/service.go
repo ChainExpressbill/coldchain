319c57f2eff0265ad8b04cbcbdf59e35a35008f5
jtpark <jtpark@deleo.co.kr>
2022-05-31 16:41:26 +0900

    feat(skip ci): login session 추가
    로그인 세션을 컨트롤하기 위한 코드 추가
    - custom middleware (authentication) 추가 필요
    - 에러 처리에 대한 소스 리팩토링 필요

--- backend/account/service.go
+++ backend/account/service.go
@@ -1,12 +1,24 @@
 package account
 
 import (
+	"github.com/ChainExpressbill/coldchain/database"
 	"github.com/ChainExpressbill/coldchain/ent"
 	"github.com/ChainExpressbill/coldchain/utils"
+	"github.com/gofiber/fiber/v2"
 )
 
-func LoginService(id, password string) *ent.Account {
-	return FindByIdAndPassword(id, utils.EncryptSHA256(password))
+func LoginService(id, password string, c *fiber.Ctx) (*ent.Account, error) {
+	account, err := FindByIdAndPassword(id, utils.EncryptSHA256(password))
+	if err != nil {
+		return nil, err
+	}
+
+	err2 := database.SetSession(account.ID, c)
+	if err2 != nil {
+		return nil, err
+	}
+
+	return account, nil
 }
 
 func LogoutService(id string) *ent.Account {
