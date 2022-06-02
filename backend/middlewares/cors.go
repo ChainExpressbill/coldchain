747dfb7e8ae3aa47b2f4533a8b7b57909aa742f0
jtpark <jtpark@deleo.co.kr>
2022-06-02 16:22:35 +0900

    feat: logger 추가
    운영 서버에서 로깅을 확인하기 위한 logger 설정(파일추가)

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func Cors() func(*fiber.Ctx) error {
+func CorsMiddleware() func(*fiber.Ctx) error {
 	return cors.New(cors.Config{
 		AllowOrigins: "*", // domain 정해지면 추가
 		AllowHeaders: "Origin, Content-Type, Accept",
