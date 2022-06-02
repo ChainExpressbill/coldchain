d2acf3320375b1d25259c114d37f4c4bbe1685c0
jtpark <vigabhan1231@gmail.com>
2022-06-03 07:16:13 +0900

    feat: login swagger 작성
    로그인 API swagger 작성

--- backend/account/handler.go
+++ backend/account/handler.go
@@ -1,22 +1,19 @@
 package account
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-// Login method to login an admin user
-// @Description Login an admin user
-// @Summary login
-// @Tags Account
-// @Accept application/json;charset=UTF-8
-// @Produce application/json;charset=UTF-8
-// @Param id body string true "Id"
-// @Param password body string true "Password"
-// @Success 200 {object} ent.Account
-// @BasePath  /
-// @Router /login [post]
+// @Description Chain expressbill 관리자 로그인
+// @Summary 	Chain expressbill 관리자 로그인
+// @Tags 			Account
+// @Accept 		application/json;charset=UTF-8
+// @Produce 	application/json;charset=UTF-8
+// @Param 		loginParams body LoginParams true "id, password"
+// @Success 	200 {object} ent.Account
+// @Failure 	400 {object} map[string]string
+// @Failure 	500 {object} map[string]string
+// @Router 		/login [post]
 func Login(c *fiber.Ctx) error {
 	params := new(LoginParams)
 
@@ -26,31 +23,50 @@ func Login(c *fiber.Ctx) error {
 
 	user, err2 := LoginService(params.Id, params.Password, c)
 	if err2 != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(map[string]string{"code": "500", "message": "login failed"})
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"code": "500", "message": "login failed"})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(user)
 }
 
+// @Description Chain expressbill 관리자 로그아웃
+// @Summary 	Chain expressbill 관리자 로그아웃
+// @Tags 			Account
+// @Accept 		application/json;charset=UTF-8
+// @Produce 	application/json;charset=UTF-8
+// @Param 		logoutParam body LoginParams true "id"
+// @Success 	200 {object} boolean
+// @Failure 	400 {object} map[string]string
+// @Failure 	500 {object} map[string]string
+// @Router 		/logout [post]
 func Logout(c *fiber.Ctx) error {
 	params := new(LoginParams)
 
 	if err := c.BodyParser(params); err != nil {
-		return c.SendStatus(fiber.StatusBadRequest)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"code": "400", "message": "id invalid"})
 	}
 
 	LogoutService(params.Id)
 	return c.SendStatus(fiber.StatusOK)
 }
 
+// @Description Chain expressbill 관리자 회원가입
+// @Summary 	Chain expressbill 관리자 회원가입
+// @Tags 			Account
+// @Accept 		application/json;charset=UTF-8
+// @Produce 	application/json;charset=UTF-8
+// @Param 		JoinParams body JoinParams true "id, password, name, emailAddress"
+// @Success 	200 {object} boolean
+// @Failure 	400 {object} map[string]string
+// @Failure 	500 {object} map[string]string
+// @Router 		/join [post]
 func Join(c *fiber.Ctx) error {
 	params := new(JoinParams)
 
 	if err := c.BodyParser(params); err != nil {
-		return c.SendStatus(fiber.StatusBadRequest)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"code": "400", "message": "join params invalid"})
 	}
 
-	fmt.Printf("Id: %s, Password: %s, Name: %s, EmailAddress: %s\n", params.Id, params.Password, params.Name, params.EmailAddress)
 	JoinService(params)
 	return c.SendStatus(fiber.StatusOK)
 }
