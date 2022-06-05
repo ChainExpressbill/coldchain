acfdcdf6daf6cb599fc603635730b90910457972
jtpark <vigabhan1231@gmail.com>
2022-06-05 12:41:44 +0900

    feat: 대시보드 요약 관련 컴포넌트 추가
    - favicon 회사 로고로 변경
    신규 컴포넌트 추가
    App.tsx에서 구조 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header, Footer } from 'components/layout';
 import { APP_STAGE } from 'constants/environment';
 import {
   LoginPage,
@@ -8,28 +7,53 @@ import {
   OrderPage,
   OrderListPage,
 } from 'pages';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 
 if (APP_STAGE === 'local') {
   require('./mocks');
 }
 
+function RequireAuth({ children }: { children: JSX.Element }) {
+  // const auth = useAuth();
+  const auth = { user: 'jtpark' };
+  const location = useLocation();
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
 function App() {
   return (
     <div className="flex flex-col w-full h-screen">
-      <Header />
-      <main className="mt-24 h-full">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/join" element={<JoinPage />} />
-            <Route path="/dashboard" element={<DashBoardPage />} />
-            <Route path="/order/:id?" element={<OrderPage />} />
-            <Route path="/orders" element={<OrderListPage />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/join" element={<JoinPage />} />
+          <Route
+            path="dashboard"
+            element={
+              <RequireAuth>
+                <DashBoardPage />
+              </RequireAuth>
+            }
+          />
+          <Route path="order/:id?" element={<OrderPage />} />
+          <Route path="orders" element={<OrderListPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/dashboard/SummaryLineChart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SummaryLineChart() {
+  return <></>;
+}
+
+export default SummaryLineChart;
