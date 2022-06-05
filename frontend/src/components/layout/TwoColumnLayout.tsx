cfce61c491840f3cb3861263bffad81ea66806bb
jtpark <vigabhan1231@gmail.com>
2022-06-05 14:54:24 +0900

    feat: 공통 레이아웃 추가
    로그인 후 화면에서 사용할 공통 레이아웃 추가(TwoColumnLayout.tsx)
    - 좌측 공통 메뉴를 위한 SideMenu 추가

--- frontend/src/components/layout/TwoColumnLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TwoColumnLayout() {
-  return <></>;
-}
-
-export default TwoColumnLayout;
---
+++ frontend/src/components/side/SideMenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SideMenu() {
+  return <aside className="lnb">left side menu</aside>;
+}
+
+export default SideMenu;
